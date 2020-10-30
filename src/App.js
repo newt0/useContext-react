4f6def7b96dad34ca1ae861495752c77c08b702a
newt0 <py_lambda@yahoo.co.jp>
2020-10-30 18:27:47 +0900

    add: custom hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import React, { useState } from "react";
-import ClassContextComponent from "./ClassContextComponent";
+import React from "react";
 import FunctionContextComponent from "./FunctionContextComponent";
+import { ThemeProvider } from "./ThemeContext";
 
 export const ThemeContext = React.createContext();
 
 export default function App() {
-  const [darkTheme, setDarkTheme] = useState(true);
-
-  function toggleTheme() {
-    setDarkTheme((prevDarkTheme) => !prevDarkTheme);
-  }
-
   return (
-    <>
-      <ThemeContext.Provider value={darkTheme}>
-        <button onClick={toggleTheme}>Toggle Theme</button>
-        <FunctionContextComponent />
-        <ClassContextComponent />
-      </ThemeContext.Provider>
-    </>
+    <ThemeProvider>
+      <FunctionContextComponent />
+    </ThemeProvider>
   );
 }
