3dcdc41cd8d3293e272ac67349bb45766b5b4f82
newt0 <py_lambda@yahoo.co.jp>
2020-10-30 18:04:35 +0900

    add: useContext in FC

--- src/FunctionContextComponent.js
+++ src/FunctionContextComponent.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "./App";
 
-function FunctionContextComponent() {
-  return <div>Function Theme</div>;
+export default function FunctionContextComponent() {
+  const darkTheme = useContext(ThemeContext);
+  const themeStyles = {
+    backgroundColor: darkTheme ? "#333" : "#CCC",
+    color: darkTheme ? "#CCC" : "#333",
+    padding: "2rem",
+    margin: "2rem",
+  };
+  return <div style={themeStyles}>Function Theme</div>;
 }
 
 export default FunctionContextComponent;
